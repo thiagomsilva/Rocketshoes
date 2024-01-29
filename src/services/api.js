c2069e60d921c51905a0f50d996aa07d3b94ef01
Thiago Magalhães <thiagomagalhaes2004@hotmail.com>
2024-01-29 20:31:18 -0300

    fix to deploy production

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,12 @@
 import axios from "axios";
 
+const baseURL =
+    process.env.NODE_ENV === "production"
+        ? "https://rocketshoes.vercel.app"
+        : "http://localhost:3333";
+
 const api = axios.create({
-    baseURL: "http://localhost:3333",
+    baseURL,
 });
 
 export default api;
