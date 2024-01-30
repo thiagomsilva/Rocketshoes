d2874b65a76a1293164ba3789402bc8968387942
Thiago Magalhães <thiagomagalhaes2004@hotmail.com>
2024-01-29 21:43:41 -0300

    Update API base URL

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const baseURL =
     process.env.NODE_ENV === "production"
-        ? "https://rocketshoes.vercel.app"
+        ? "https://my-json-server.typicode.com/thiagomsilva/server"
         : "http://localhost:3333";
 
 const api = axios.create({
